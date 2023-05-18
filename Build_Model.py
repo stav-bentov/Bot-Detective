6f8b871da34e760e1bd361facc03ca816be25249
stav-bentov <stavbentov5@gmail.com>
2023-05-18 21:41:17 +0300

    grid search added

--- Build_Model.py
+++ Build_Model.py
@@ -2,7 +2,67 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 import pickle # for saving the model
+from sklearn.model_selection import GridSearchCV
+from sklearn.naive_bayes import GaussianNB
+from sklearn.calibration import LinearSVC
+import numpy as np
 
+def grid_search_nb(X, Y):
+    nb = GaussianNB()
+    grid_space={
+        'var_smoothing': np.logspace(0,-9, num=100)
+           }
+        
+    grid = GridSearchCV(estimator = nb, param_grid = grid_space, verbose = 1, cv = 5, n_jobs = -1, scoring ='accuracy')
+    model_grid = grid.fit(X, Y)
+
+    print('Best hyperparameters are: '+str(model_grid.best_params_))
+    print('Best score is: '+str(model_grid.best_score_))
+    return model_grid.best_score_
+
+def grid_search_rf(X, Y):
+    print("in rf")
+    rf = RandomForestClassifier()
+    grid_space= {'n_estimators': [100, 200, 300, 400, 500], 
+                 'max_depth': [5, 10, 15, 20], 
+                 'min_samples_split': [2, 5, 10]}
+        
+    grid = GridSearchCV(estimator = rf,param_grid = grid_space,cv = 5, scoring ='accuracy')
+    model_grid = grid.fit(X, Y)
+
+    print('Best hyperparameters are: '+str(model_grid.best_params_))
+    print('Best score is: '+str(model_grid.best_score_))
+    print("end rf")
+    return model_grid.best_score_
+
+def grid_search_lsvc(X, Y):
+    param_grid_linearSVC = { 'C': [0.1, 1, 10, 100, 1000], 'max_iter': [3000, 4000, 5000]}
+    # Create a based model
+    linearSVC = LinearSVC(random_state = 1)
+    # Instantiate the grid search model
+    grid = GridSearchCV(estimator = linearSVC,
+                                param_grid = param_grid_linearSVC,
+                                cv = 5, n_jobs = -1,
+                                scoring='accuracy',
+                                verbose = 2)
+    # run the grid search
+    model_grid = grid.fit(X, Y)
+
+    print('Best hyperparameters are: '+str(model_grid.best_params_))
+    print('Best score is: '+str(model_grid.best_score_))
+    return model_grid.best_score_
+
+def find_best_model(X, Y):
+    models = {}
+    models["Gaussian Naive Bayes"] = grid_search_nb(X, Y)
+    print("Done Gaussian Naive Bayes")
+    models["Random forest"] = grid_search_rf(X, Y)
+    print("Done Random forest")
+    models["Linear SVC"] = grid_search_lsvc(X, Y)
+    print("Done Linear SVC")
+    
+    best_model = max(models, key=models.get)
+    print("The best model is: ",best_model, "with accuracy of: ", models[best_model])
 
 def create_and_save_model():
     """
@@ -15,10 +75,11 @@ def create_and_save_model():
     # split features and target
     X = df.drop('target', axis=1)
     Y = df['target']
-
+    
+    #find_best_model(X, Y)
+    #grid_search_rf(X, Y)
     # train the model
-    model = RandomForestClassifier(n_estimators = 100, max_depth = 5, random_state = 1)
-
+    model = RandomForestClassifier(n_estimators = 200, max_depth = 5, random_state = 1)
     model.fit(X, Y)
 
     # calc 5 fold cross validation test
