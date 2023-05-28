f44c5aa3272ca525c24e9dd0c39010e1a35f7fd4
stav-bentov <stavbentov5@gmail.com>
2023-05-28 18:06:01 +0300

    updated fastapi

--- API_requests.py
+++ API_requests.py
@@ -2,17 +2,13 @@ from typing import Union
 from Detector import detect_user_model
 from Detector import load_model
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi import FastAPI 
+import uvicorn
 
 model = load_model() # load the model once
+
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Update this with the appropriate origins
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 def read_root():
@@ -24,3 +20,13 @@ async def is_bot(username: str):
 	# The return value from a function in a Flask app should be JSON serializable.
     result = 1 if detect_user_model(model, username) else 0 # 1 - bot, 0 - not bot
     return {username: result}
+
+app.add_middleware(HTTPSRedirectMiddleware)  # Redirect HTTP to HTTPS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Update this with the appropriate origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+uvicorn.run(app, host="0.0.0.0", port=3003)
