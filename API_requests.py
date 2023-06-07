7575a38ad936f4bd5016fad29588cd4e472d4ab8
Tamir Davidi <79422317+tdavidi10@users.noreply.github.com>
2023-06-07 23:27:13 +0300

    redis + manifest + icon

--- API_requests.py
+++ API_requests.py
@@ -5,6 +5,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from fastapi import FastAPI 
 import uvicorn
+import redis
+
+# creating redis storage to store the results of the model from all users ever calculated
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 model = load_model() # load the model once
 
@@ -17,9 +21,32 @@ def read_root():
 @app.get("/isBot/{usernames_str}")
 async def is_bot(usernames_str: str):
     usernames_list = usernames_str.split(",")
+    print("len before remove (usernames_list) = {0}".format(len(usernames_list)))
+    result = {}
+    # update usernames_list to be only the usernames that are not in the redis storage
+    for username in usernames_list:
+        if r.get(username) is not None: # if the username is in the redis storage
+            result[username] = r.get(username) # get the result from redis storage
+            usernames_list.remove(username) # remove the username from the list of usernames to be calculated
 	# The return value from a function in a Flask/FastAPI app should be JSON serializable.
-    result = detect_users_model(model, usernames_list)
-    print(result)
+    #print("usernames_list = {0}".format(usernames_list))
+    print("len after remove (usernames_list) = {0}".format(len(usernames_list)))
+    if 1 <= len(usernames_list) <= 100:
+        result.update(detect_users_model(model, usernames_list)) 
+    else:
+        print("error: len(usernames_list) = {0}".format(len(usernames_list)))
+        print("usernames_list = {0}".format(usernames_list))
+        print("can't be more than 100 usernames and less than 1 username")
+
+    # update redis storage with the new usernames and their results
+    for username in usernames_list:
+        if username in result:
+            r.set(username, result[username])
+        else:
+            print("error: username =, " + username + "is not in result")
+            print("result = {0}".format(result))
+
+    #print(result)
     return result
 
 #app.add_middleware(HTTPSRedirectMiddleware)  # Redirect HTTP to HTTPS
