2abb1df03997eb25a7a747731397fee71291093b
Tamir Davidi <79422317+tdavidi10@users.noreply.github.com>
2023-06-11 23:44:17 +0300

    Redis expiration time

--- API_requests.py
+++ API_requests.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import Union
 from Detector import detect_users_model
 from Detector import load_model
@@ -8,6 +9,8 @@ import uvicorn
 import redis
 
 # Creating redis storage to store the results of the model from all users ever calculated
+# values are stored in the following format:
+# {username: {'classification': result, 'expiration': expirationDate}}
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 model = load_model() # load the model once
@@ -30,14 +33,16 @@ async def is_bot(usernames_str: str):
         # If the username is in the redis storage:
         # 1. Get the result from redis storage
         # 2. Remove the username from the list of usernames that need to be calculated (by the model)
-        print("in loop for: {0}".format(username))
         if r.get(username) is not None:
-            result[username] = r.get(username) 
-            usernames_list.remove(username) 
+            userStorageValue = eval(r.get(username)) # convert string to userStorageValue dict
+            expirationDate = datetime.datetime.strptime(str(userStorageValue['expiration']), '%Y-%m-%d %H:%M:%S.%f')
+            if expirationDate > datetime.datetime.now(): # redis value is still valid (has not expired yet)
+                result[username] = userStorageValue['classification'] 
+                usernames_list.remove(username) 
     print("len after remove (usernames_list) = {0}".format(len(usernames_list)))
     
     # Calculates users in model and adds to the result
-    if usernames_list:
+    if len(usernames_list) > 0: # cant send 0 users to model
         print("usernames_list: {0}".format(usernames_list))
         result.update(detect_users_model(model, usernames_list))
     else:
@@ -45,9 +50,10 @@ async def is_bot(usernames_str: str):
 
     # Update redis storage with the **new** usernames and their results
     for username in usernames_list:
-        print("update redis or:{0}".format(username))
-        r.set(username, result[username])
-
+        expirationDate = datetime.datetime.now() + datetime.timedelta(days=30) # 30 days from now
+        userStorageValue = {'classification': result[username], 'expiration': expirationDate}
+        userStorageValue = str(userStorageValue) # convert dict to string according to redis storage format
+        r.set(username, userStorageValue)
     return result
 
 #app.add_middleware(HTTPSRedirectMiddleware)  # Redirect HTTP to HTTPS
