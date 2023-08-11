b6d5d487f65399c903e0ef485b7be4fec4b66a41
Tamir Davidi <79422317+tdavidi10@users.noreply.github.com>
2023-08-12 00:44:19 +0300

    knesset bots

--- Knesset_Bots.py
+++ Knesset_Bots.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 from Detector import send_Twitter_API_request
 from Detector import get_features
 from Detector import model_predict_if_user_is_bot
@@ -15,13 +16,25 @@ knesset_members_by_party = {"Halicod":["avidichter","AvichayBuaron","OfirAkunis"
 ,"HaziyonutHadatit":["MKOhadTal","ofir_sofer","oritstrock","michalwoldiger","Moshe_solomon_","tzvisuccot","rothmar"]
 ,"HamahaneHamamlachti":["FarkashOrit","alonschuster_mk","gantzbe","eisenkotgadi","gidonsaar","zeev_elkin","hilitrooper","MichaelBiton4","MatanKahana","pnina_tamano_sh","SharrenHaskel"]
 ,"Shas":["BussoUriel","ariyederi","haiimaemek","yossitaieb","malkielim82","Abutbulm"]
-,"YahadutHatora":["LBrwky","Pikeaccount","TESLERYANKY","YisraelEichler","Gafni_Moshe"]
+,"YahadutHatora":["LBrwky","Pikeaccount","TESLERYANKY","YisraelEichler","mk_moshe_gafni"]
 ,"IsraelBeitenu":["AvigdorLiberman","Hamad_Ammar_","evgenysova","YuliaMalinovsky","oded_forer","SharonNir11"]
 ,"HadashTaal":["Ahmad_tibi","AyOdeh","yousefataw","AidaTuma","ofercass"]
-,"raam":["khatib_eman","WAlhwashla","Waleedt68","mnsorabbas"]
+,"Raam":["khatib_eman","WAlhwashla","Waleedt68","mnsorabbas"]
 ,"Haavoda":["Efratrayten1","KarivGilad","MeravMichaeli","naamalazimi"]
 ,"OtzzmaYehudit":["itamarbengvir","almog_cohen08","Yitzik_kroizer","ItshakWaserlauf","limor_sonhrmelh","tzvikafoghel"]
 ,"Noam":["noamparty"]}
+
+party_leaders = ["Netanyahu", "yairlapid", "bezalelsm", "gantzbe", "ariyederi", "Yitzhak_Goldkno", "AvigdorLiberman", "mnsorabbas", "AyOdeh", "MeravMichaeli", "itamarbengvir"]
+
+coalition_members = []
+oposition_members = []
+for party in knesset_members_by_party:
+    if party in ["Halicod", "HaziyonutHadatit", "Shas", "YahadutHatora", "OtzzmaYehudit", "Noam"]: # coalition parties
+        coalition_members += knesset_members_by_party[party]
+    else:
+        oposition_members += knesset_members_by_party[party]
+
+ministers = ['bezalelsm', 'Netanyahu', 'Israel_katz', 'yoavgallant', 'itamarbengvir', 'Yitzhak_Goldkno', 'ariyederi', 'iditsilman', 'GalitDistel', 'oritstrock', 'OfirAkunis', 'elicoh1', 'YoavKisch', 'avidichter', 'Eliyahu_a', 'aORLB0U686MznSE', 'NirBarkat', 'GilaGamliel', 'AmbDermer', 'BentzurYoav', 'ofir_sofer', 'ItshakWaserlauf', 'GolanMay', 'yakmargi', 'AmichaiChikli', 'malkielim82', 'dudiamsalem', 'regev_miri', 'shlomo_karhi', 'zoharm7']
 
 knesset_members = []
 for party in knesset_members_by_party:
@@ -30,6 +43,7 @@ for party in knesset_members_by_party:
 
 number_of_followers_requests = 0 # rate limit: 15 requests per 15 minutes
 number_of_user_lookup_requests = 0 # rate limit: 900 requests per 15 minutes
+
 def get_bot_percentage_in_followers(model, username): # now taking all followers, not just 100
     """
         Input: model- The model that classify our users
@@ -81,9 +95,8 @@ def get_bot_percentage_in_followers(model, username): # now taking all followers
     if bot_prec[0] + bot_prec[1] == 0: # no followers - avoid division by zero
         return 0
     bot_percentage_of_followers = (bot_prec[1] / (bot_prec[0] + bot_prec[1])) * 100
-    print("res: ", res) # TODO: remove (DEBUG ONLY)
+    #print("res: ", res) # TODO: remove (DEBUG ONLY)
     return bot_percentage_of_followers
-
 """
         Input: model- The model that classify our users
                knesset_members- The usernames of the knesset members whose followers we want to examine
@@ -91,19 +104,148 @@ def get_bot_percentage_in_followers(model, username): # now taking all followers
 """
 def get_knesset_members_bots_percentage(model, knesset_members):
     knesset_members_bots_percentage = {}
-    for knesset_member in knesset_members:
+    for knesset_member in enumerate(knesset_members):
         knesset_members_bots_percentage[knesset_member] = get_bot_percentage_in_followers(model, knesset_member)
     return knesset_members_bots_percentage
 
+######### Builds the file "all_knesset_members_bots_percentage.txt" which contains the percentage of bots among the followers of each knesset member #########
+def create_results_file_for_each_knesset_member(model, knesset_members):
+    for knesset_member in knesset_members:
+        with open(f"{knesset_member}_bots_percentage.txt", "w") as file:
+            file.write(str(get_knesset_members_bots_percentage(model, [knesset_member])))
+            
+def create_result_file_for_all_knesset_members(knesset_members):
+    # unite the results from all results files into one file (the files are in "knesset" folder)
+    with open("all_knesset_members_bots_percentage.txt", "w") as file:
+        for knesset_member in knesset_members:
+            with open(f"knesset/{knesset_member}_bots_percentage.txt", "r") as knesset_member_file:
+                file.write(knesset_member_file.read())
+                file.write("\n")
+
+############ ran one time just for build DB of knesset members bots percentage ############
+
+# return a dictionary with keys: kneset members usernames and values: percentage of bots among their followers
+# knesset_member: bot_percentage_of_his_followers
+def read_knesset_bot_percentage_file_to_dictionary():
+    results = {}
+    with open("knesset/all_knesset_members_bots_percentage.txt", "r") as file:
+        for line in file:
+            data_dict = eval(line)
+            # Merge the data_dict into result_dict
+            results.update(data_dict)
+    return results
+
+# return a dictionary in format -> minister: bot_percentage_of_his_followers
+def read_ministers_bot_percentage_file_to_dictionary():
+    results = {}
+    with open("knesset/ministers_bot_percentage.txt", "r") as file:
+        for line in file:
+            key, value = line.strip().split(': ')
+            value = float(value)
+            results[key] = value
+
+    return results
+
+# return a dictionary-> party: bot_percentage_of_its_members (average)
+def get_parties_bot_percentages(knesset_members_bot_percentages):
+    parties_bot_percentages = {} # party: bot_percentage_of_its_members (average)
+    for party in knesset_members_by_party:
+        parties_bot_percentages[party] = 0
+        for knesset_member in knesset_members_by_party[party]:
+            parties_bot_percentages[party] += knesset_members_bot_percentages[knesset_member]
+        parties_bot_percentages[party] /= len(knesset_members_by_party[party])
+    return parties_bot_percentages
+
+def plot_parties_bot_percentages(knesset_members_bot_percentages):
+    parties_bot_percentages = get_parties_bot_percentages(knesset_members_bot_percentages) # party: bot_percentage_of_its_members (average)
+    plt.bar(range(len(parties_bot_percentages)), list(parties_bot_percentages.values()), align='center')
+    plt.xticks(range(len(parties_bot_percentages)), list(parties_bot_percentages.keys()))
+
+    plt.xticks(rotation=90)
+    # add on top of each bar its value
+    for i, v in enumerate(parties_bot_percentages.values()):
+        plt.text(i - 0.5, v + 0.5, str(round(v, 2))+"%", color='blue', size=8)
+
+    plt.title("Parties Bot Percentages")
+    plt.xlabel("Party")
+    plt.ylabel("Bot Percentage")
+    plt.show()
+
+# Write to knesset/ministers_bot_percentage.txt the bot percentage of each minister:
+def create_results_file_of_ministers(model, knesset_members_bot_percentages):
+    with open("knesset/ministers_bot_percentage.txt", "w") as file:
+        for minister in ministers:
+            # if the minister already calculated -> write his bot percentage
+            if minister in knesset_members_bot_percentages:
+                file.write(f"{minister}: {knesset_members_bot_percentages[minister]}\n")
+            else:
+                # else -> calculate his bot percentage using get_bot_percentage_in_followers(   )
+                print(f"calculating bot percentage of {minister}...")
+                file.write(f"{minister}: {get_bot_percentage_in_followers(model, minister)}\n")
+
+def plot_ministers_bot_percentages(ministers_bot_percentages):
+    # sort the dictionary by values reversed (bot percentages)
+    ministers_bot_percentages = dict(sorted(ministers_bot_percentages.items(), key=lambda item: item[1], reverse=True))
+    plt.bar(range(len(ministers_bot_percentages)), list(ministers_bot_percentages.values()), align='center')
+    plt.xticks(range(len(ministers_bot_percentages)), list(ministers_bot_percentages.keys()))
+    plt.xticks(rotation=90)
+    # add on top of each bar its value
+    for i, v in enumerate(ministers_bot_percentages.values()):
+        plt.text(i - 0.5, v + 0.5, str(round(v, 2))+"%", color='blue', size=8)
+
+    plt.title("Ministers Bot Percentages")
+    plt.xlabel("Minister")
+    plt.ylabel("Bot Percentage")
+    plt.show()
+
+def plot_coalition_oposition_bot_percentage():
+    coalition_bot_percentage = 0
+    for knesset_member in coalition_members:
+        coalition_bot_percentage += knesset_members_bot_percentages[knesset_member]
+    coalition_bot_percentage /= len(coalition_members)
+
+    oposition_bot_percentage = 0
+    for knesset_member in oposition_members:
+        oposition_bot_percentage += knesset_members_bot_percentages[knesset_member]
+    oposition_bot_percentage /= len(oposition_members)
+
+    plt.bar(["Coalition", "Oposition"], [coalition_bot_percentage, oposition_bot_percentage], align='center')
+    plt.xticks(["Coalition", "Oposition"])
+    plt.xticks(rotation=90)
+    # add on top of each bar its value
+    for i, v in enumerate([coalition_bot_percentage, oposition_bot_percentage]):
+        plt.text(i - 0.1, v + 0.5, str(round(v, 2))+"%", color='blue', size=8)
+
+    plt.title("Coalition Vs. Oposition Bot Percentages")
+    plt.xlabel("Coalition Vs. Oposition")
+    plt.ylabel("Bot Percentage")
+    plt.show()
+        
+def plot_party_leaders_bot_percentage():
+    party_leaders_bot_percentage = {} # party: bot_percentage_of_its_members (average)
+    for party_leader in party_leaders:
+        if party_leader in knesset_members_bot_percentages: # if the party leader is a knesset member
+            party_leaders_bot_percentage[party_leader] = knesset_members_bot_percentages[party_leader]
+        else: # he is a minister and not a knesset member
+            party_leaders_bot_percentage[party_leader] = ministers_bot_percentages[party_leader]
+    # sort the dictionary by values reversed (bot percentages)
+    party_leaders_bot_percentage = dict(sorted(party_leaders_bot_percentage.items(), key=lambda item: item[1], reverse=True))
+    plt.bar(range(len(party_leaders_bot_percentage)), list(party_leaders_bot_percentage.values()), align='center')
+    plt.xticks(range(len(party_leaders_bot_percentage)), list(party_leaders_bot_percentage.keys()))
+    plt.xticks(rotation=90)
+    # add on top of each bar its value
+    for i, v in enumerate(party_leaders_bot_percentage.values()):
+        plt.text(i - 0.5, v + 0.5, str(round(v, 2))+"%", color='blue', size=8)
+    plt.title("Party Leaders Bot Percentages")
+    plt.xlabel("Party Leader")
+    plt.ylabel("Bot Percentage")
+    plt.show()
 
 
-# example: (DEBUG ONLY)
-print(get_knesset_members_bots_percentage(model, ["tuviapeled"]))   
+knesset_members_bot_percentages = read_knesset_bot_percentage_file_to_dictionary() # knesset_member: bot_percentage_of_his_followers
+ministers_bot_percentages = read_ministers_bot_percentage_file_to_dictionary() # minister: bot_percentage_of_his_followers
 
-# real run:
-# print(get_knesset_members_bots_percentage(model, knesset_members))
-
-
-
-
-
+#plot_parties_bot_percentages(knesset_members_bot_percentages)
+#plot_ministers_bot_percentages(ministers_bot_percentages)
+#plot_coalition_oposition_bot_percentage()
+#plot_party_leaders_bot_percentage()
